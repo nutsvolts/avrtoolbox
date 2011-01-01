a169d6d9645adfa6bc84a475dec0f6c999bf77f4
smiley.micros <smiley.micros@gmail.com>
2011-01-01 22:07:13 +0000

    Testing keyword substitution.

---
+++ libraries/elementary/serial/keyword_substitution_test.h
@@ -0,0 +1,23 @@
+key_word_substitution_test.txt
+
+<!---
+/* ********************************************************** */
+	
+Repository path:    $HeadURL$
+Last committed:     $Revision$
+Last changed by:    $Author$
+Last changed date:    $Date$
+ID:            $Id$
+
+/* ********************************************************** */
+--->
+
+<!---
+=================================================================================================
+Repository path:    $HeadURL$
+Last committed:     $Revision$
+Last changed by:    $Author$
+Last changed date:    $Date$
+ID:            $Id$
+=================================================================================================
+--->
