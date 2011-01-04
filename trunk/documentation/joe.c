b2b8e5a5faf788385fdbafbd27e5e2e38df80251
smiley.micros@gmail.com <smiley.micros@gmail.com@04f8c15a-18a7-9803-21a3-6a6cebf7e57b>
2011-01-04 21:41:18 +0000

    testing keyword substitution

---
+++ trunk/documentation/joe.c
@@ -0,0 +1,15 @@
+joe.c
+
+<!---
+/* ********************************************************** */
+	
+Repository path:    $HeadURL$
+Last committed:     $Revision$
+Last changed by:    $Author$
+Last changed date:    $Date$
+ID:            $Id$
+
+/* ********************************************************** */
+--->
+
+
