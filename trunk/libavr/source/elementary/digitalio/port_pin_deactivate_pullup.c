760ffecaa5be4b7e38facccb3d3214c25e5a1381
smiley.micros@gmail.com <smiley.micros@gmail.com@04f8c15a-18a7-9803-21a3-6a6cebf7e57b>
2011-10-03 21:41:25 +0000

    Finished libavr_atmega328_003.a and the digitalio testers.

---
+++ trunk/libavr/source/elementary/digitalio/port_pin_deactivate_pullup.c
@@ -0,0 +1,83 @@
+/*
+ *  BSD License
+ *  -----------
+ *
+ *  Copyright (c) 2011, and Joe Pardue, All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer. 
+ *   
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ *   
+ *  - Neither the name of Joe Pardue nor the names of 
+ *    its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission. 
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+ // Some of this code may have elements taken from other code without attribution.
+ // If this is the case it was due to oversight while debugging and I apologize.
+ // If anyone has any reason to believe that any of this code violates other licenses
+ // please contact me with details so that I may correct the situation. 
+
+#include "digitalio.h"
+int8_t port_pin_deactivate_pullup(uint8_t portx, uint8_t pin)
+{
+#if defined (__AVR_ATmega169__) || defined (__AVR_ATmega169P__)
+	if (portx == PORTB) 
+	{
+		bit_clear(DDRB,pin); // Make sure it is an input
+		bit_clear(PORTB,pin);  // Deactivate pullup
+	}
+	else if (portx == PORTC)
+	{
+		bit_clear(DDRC,pin); // Make sure it is an input
+		bit_clear(PORTC,pin);  // Deactivate pullup
+	}
+	else // out of range 
+	{
+		return(-1); // return ERROR
+	}
+	// TODO: remaining accessible Butterfly ports
+#elif defined (__AVR_ATmega328__) || defined (__AVR_ATmega328P__) 
+	if( portx == PORTD ) 
+	{
+		bit_clear(DDRD,pin); // Make sure it is an input
+		bit_clear(PORTD,pin);  // Deactivate pullup
+	}
+	else if (portx == PORTB)
+	{
+		bit_clear(DDRB,pin); // Make sure it is an input
+		bit_clear(PORTB,pin);  // Deactivate pullup
+	}
+	else if (portx == PORTC)
+	{ 		
+		bit_clear(DDRC,pin); // Make sure it is an input
+		bit_clear(PORTC,pin);  // Deactivate pullup
+	}
+		else // out of range 
+	{
+		return(-1); // return ERROR
+	}	
+#else 
+#    warning "device type not defined"
+#endif
+	return(1); // return OKAY
+
+}
